677b721258016c77fd4e010ba9782dd6b610da5f
ruthssoll <ruths9317@gmail.com>
2022-06-11 22:49:10 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
     <div className="main">
       <Sidebar />
       <div className="container">
+        <input className='search' type='text' placeholder='Search...'></input>
+        <button className='btnsearch'>Search</button>
         <h1 className="title">My React App</h1>
         <p className="info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         <button className="btn">Explore now</button>
